c3173ce29d8cab09dea28eed3dbe2fd4e3b407cb
Naveed <naveedfarooq692@gmail.com>
2024-10-15 12:40:03 +0500

    setting update

--- FazalSons/settings.py
+++ FazalSons/settings.py
@@ -138,11 +138,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(PRODUCT_DIR, "static/")
 STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(PRODUCT_DIR, "static"), '/var/www/static/',
-]
+# STATICFILES_DIRS = [
+#     os.path.join(PRODUCT_DIR, "static"), '/var/www/static/',
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
