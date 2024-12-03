a8b35c813ac2346ac1e42f97dabd1c4d2646a5c9
Naveed <naveedfarooq692@gmail.com>
2024-12-03 12:11:26 +0500

    Update Fetch Outler

--- AppProduct/views.py
+++ AppProduct/views.py
@@ -61,8 +61,8 @@ def FetchOutletView(request):
     outlets = Outlet.objects.all()
     if outlets.exists():
         serializer = OutletSerializer(outlets, many=True)
-        return Response(serializer.data)
-    return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    return Response(status=status.HTTP_200_OK)
 
 
 ### BRAND VIEW
